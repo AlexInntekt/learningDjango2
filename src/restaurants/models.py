4a1ef120d979ca912b34d4cc8a0e11dffa0dc602
unknown <Alex>
2018-07-26 18:07:06 +0300

    Lecture 15 - More on Model Fields Playing with some forms of categories in the model

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 # Create your models here.
 class RestaurantsLocation(models.Model):
-   name = models.CharField(max_length=120)
-   location = models.CharField(max_length=120, null=True, blank=True)
+   name          = models.CharField(max_length=120)
+   location      = models.CharField(max_length=120, null=True, blank=True)
+   
+   category      = models.CharField(max_length=120, null=True, blank=True)
+   time_stamp    = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
+   updated       = models.DateTimeField(auto_now=True)
+   #my_date_field = models.DateField(auto_now=False, auto_now_add=False)
