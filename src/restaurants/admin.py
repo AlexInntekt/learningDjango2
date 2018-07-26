d0884af136b43cde0720c068fa9e04e4e3b7f819
unknown <Alex>
2018-07-26 16:29:33 +0300

    Lecture 14
    Creating a model for saving server-side data, making migrations and some set-up

--- src/restaurants/admin.py
+++ src/restaurants/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import RestaurantsLocation
+#from .models import Post
+#from .models import Comment
+
+admin.site.register(RestaurantsLocation)
+
+#admin.site.register(Post)
+#admin.site.register(Comment)
